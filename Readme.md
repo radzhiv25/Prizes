2f5607815bc2ca9b1026a3085ea6aca5af85d45d
radzhiv <radzhivkrishna@gmail.com>
2023-11-17 12:00:02 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+### Assignment
+
+## For Sorting prizes based on the given API
+
+## Techstack used: HTML, TailwindCSS and Vanilla Javascript
---
+++ script.js
@@ -0,0 +1,37 @@
+const showDataEl = document.getElementById('show-data')
+
+const yearOptionEl = document.getElementById('year-option')
+const categoryOptionEl = document.getElementById('category-option')
+
+const newArr =[]
+async function prizeData(){
+    const res = await axios.get('https://api.nobelprize.org/v1/prize.json')
+    const data = res.data.prizes
+    data.map((year) => {if(year.year >= 1900 && year.year <= 2018){
+        newArr.push(year)
+    }})
+    console.log(newArr)
+    newArr.forEach((year) => {
+        const option = document.createElement('option')
+        option.textContent = newArr.filter((year) => year.year)
+        yearOptionEl.appendChild(option)
+        if(year.year == year.year){
+            const p = document.createElement('p')
+
+            for(let i = 0; i < year.laureates.length; i++){
+                p.textContent = year.laureates[i].firstname
+                showDataEl.appendChild(p)
+            }
+            console.log(year.laureates.firstname)
+        }
+    })
+    newArr.forEach((category) => {
+        const option = document.createElement('option')
+        option.textContent = category.category
+        categoryOptionEl.appendChild(option)
+    })
+    
+}
+prizeData()
+
+
